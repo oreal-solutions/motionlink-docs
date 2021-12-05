d6964cce1b1bb098061fd8a3b4914d202bb2b3d8
Batandwa Mgutsi <b.mgutsi@gmail.com>
2021-12-05 15:02:36 +0200

    Updates meta tag for index

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Home`}
-      description="Description will go into a meta tag in <head />"
+      title={`MotionLink`}
+      description="Publish content to your blog, portfolio and doc websites from Notion"
     >
       <HomepageHeader />
       <main>
