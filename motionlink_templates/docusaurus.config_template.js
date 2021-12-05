9354ff69f2f052fa8248f9fa109066520d2a0714
Batandwa Mgutsi <b.mgutsi@gmail.com>
2021-12-05 14:46:56 +0200

    Adds analytics

--- motionlink_templates/docusaurus.config_template.js
+++ motionlink_templates/docusaurus.config_template.js
@@ -39,9 +39,17 @@ const config = {
     ],
   ],
 
+  plugins: ["@docusaurus/plugin-google-analytics"],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      googleAnalytics: {
+        trackingID: "G-V7RLV8MYRV",
+        // Optional fields.
+        anonymizeIP: true, // Should IPs be anonymized?
+      },
+
       navbar: {
         title: "MotionLink",
         items: [
