29d5f2c466158c2a74301d5706117dd88225d2a6
Batandwa Mgutsi <b.mgutsi@gmail.com>
2021-12-28 02:25:42 +0200

    Adds MotionLink pricing localiser

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -1,35 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "@theme/Layout";
 import styles from "./pricing.module.css";
 import Link from "@docusaurus/Link";
+import useScript from "../hooks/use_script";
 
-const plans = [
-  {
-    name: "Starter",
-    description: "Create up to 1 link",
-    bill: "Free",
-  },
-  {
-    name: "Writer",
-    description: "Create up to 10 links",
-    bill: "R149 billed monthly",
-  },
-  {
-    name: "Publisher",
-    description: "Create up to 50 links",
-    bill: "R599 billed monthly",
-  },
-];
+function PlansGrid() {
+  const [plans, setPlans] = useState([]);
+  useEffect(async () => {
+    const inPlans = await window.getLocalisedPlans();
+    setPlans(inPlans);
+  });
 
-export default function Pricing() {
-  return (
-    <Layout title={"Pricing Information"} description={"Pricing plans here"}>
-      <div className={`container ${styles.pricingContainer}`}>
-        {plans.map((plan) => (
+  return plans.length === 0 ? (
+    <h2>Loading...</h2>
+  ) : (
+    <div className={`container ${styles.pricingContainer}`}>
+      {plans.map((plan) => {
+        const bill =
+          plan.zarAmount === 0
+            ? "Free"
+            : `${plan.localPriceWithSymbol} billed monthly`;
+
+        return (
           <div key={plan.name} className={styles.pricingCard}>
             <div className={styles.pricingCardContent}>
               <p className={styles.pricingCardTitleText}>{plan.name}</p>
-              <p className={styles.pricingCardPriceText}>{plan.bill}</p>
+              <p className={styles.pricingCardPriceText}>{bill}</p>
               <p className={styles.pricingCardDescriptionText}>
                 {plan.description}
               </p>
@@ -41,8 +37,20 @@ export default function Pricing() {
               </Link>{" "}
             </div>
           </div>
-        ))}
-      </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default function Pricing() {
+  const scriptStatus = useScript(
+    "https://motionlink-pricing-localiser.web.app/main.js"
+  );
+
+  return (
+    <Layout title={"Pricing Information"} description={"Pricing plans here"}>
+      {scriptStatus === "ready" && <PlansGrid />}
     </Layout>
   );
 }
