d5184efe19b05e43e1485f7f4a1a30e85655f204
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-12-05 11:10:24 +0200

    Show shop button

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,9 +24,9 @@ function HomepageHeader() {
           <div className={styles.buttonSpacer}></div>
           <Link
             className="button button--secondary button--lg"
-            to="https://github.com/marketplace/motionlink"
+            to="https://paystack.shop/motionlink-templates"
           >
-            Install
+            Shop
           </Link>
         </div>
         <div className={styles.buttons}>
