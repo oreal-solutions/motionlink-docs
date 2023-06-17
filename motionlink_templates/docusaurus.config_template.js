438ea1e1f403a04703945c4ff90bfb4969bcc9cb
Batandwa Mgutsi <batandwamgutsi@Batandwas-MacBook-Pro.local>
2023-06-17 16:28:11 +0200

    Update analytics domain

--- motionlink_templates/docusaurus.config_template.js
+++ motionlink_templates/docusaurus.config_template.js
@@ -19,7 +19,7 @@ const config = {
 
   scripts: [
     {
-      src: "https://sl.motionlink.co/js/script.js",
+      src: "https://plausible.io/js/script.js",
       defer: true,
       "data-domain": "motionlink.co",
     },
